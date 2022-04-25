c08c6faa34f837e2cabc7be3375234efd5cf4245
LuytBui <luytbq43@gmail.com>
2022-04-25 09:58:53 +0700

    done e1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FontSizeEditorComponent,
   ],
     imports: [
         BrowserModule,
